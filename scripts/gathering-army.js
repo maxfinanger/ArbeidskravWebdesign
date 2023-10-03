8d1d82eaafd6b6e01f0fa48547c2eff442a522aa
Max Dyrø Finanger <max.d.finanger@gmail.com>
2023-10-03 14:25:42 +0200

    Added functional buttons to the gathering-army page.  Added comments explaining the code.  Added a new css file to style the buttons.  Added a new js file to handle the button clicks.  Added to html file to display the buttons.

--- scripts/gathering-army.js
+++ scripts/gathering-army.js
@@ -1,29 +1,26 @@
 import WarriorModule from "./Modules/WarriorModule.js";
 
-const warrior1Btn = document.getElementById("warrior-button-101");
-const warrior2Btn = document.getElementById("warrior-button-102");
-const warrior3Btn = document.getElementById("warrior-button-103");
-const warrior4Btn = document.getElementById("warrior-button-104");
-const warrior5Btn = document.getElementById("warrior-button-105");
-const warrior6Btn = document.getElementById("warrior-button-106");
 const warriorDiv = document.getElementById("warrior-div");
 const warriors = WarriorModule.getAll();
 console.log(warriors);
 
+// shows all warriors in the warrior div
 const showWarriors = () => {
     let allWarriorsHTML = "";
 
     warriors.forEach((warrior) => {
         const warriorHTML = `
         <article class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
-        <div class="shadow border border-rounded p-1 card h-100 d-flex flex-column">
-            <img src="images/${warrior.img}" alt="${warrior.name}" />
-            <h2>${warrior.name}</h2>
-            <p>Health: ${warrior.health}</p>
-            <button class="mt-auto" id"warrior-button-${warrior.id}">Buy for ${warrior.price} gold</button>
-        </div>
-    </article>
-    `;
+            <div class="shadow border border-rounded p-1 card h-100 d-flex flex-column justify-content-end">
+                <div class="card-content mb-auto">
+                    <img src="images/${warrior.img}" alt="${warrior.name}" class="card-img-top warrior-image">
+                    <h2>${warrior.name}</h2>
+                    <p>Health: ${warrior.health}</p>
+                </div>
+                <button class="mt-auto btn bg-danger text-white" id="warrior-button-${warrior.id}">Buy for ${warrior.price} gold</button>
+            </div>
+        </article>
+        `;
 
         allWarriorsHTML += warriorHTML;
     });
@@ -31,35 +28,32 @@ const showWarriors = () => {
     warriorDiv.innerHTML = allWarriorsHTML;
 };
 
+// gets a warrior by id and adds it to the army
 const getWarrior = (id) => {
     const warriors = WarriorModule.getAll();
     const warrior = warriors.find((warrior) => warrior.id === id);
     console.log(warrior);
 };
 
-const setEvents = () => {
-    warrior1Btn.addEventListener("click", () => {
-        getWarrior(101);
-    });
-    warrior2Btn.addEventListener("click", () => {
-        getWarrior(102);
-    });
-    warrior3Btn.addEventListener("click", () => {
-        getWarrior(103);
-    });
-    warrior4Btn.addEventListener("click", () => {
-        getWarrior(104);
-    });
-    warrior5Btn.addEventListener("click", () => {
-        getWarrior(105);
-    });
-    warrior6Btn.addEventListener("click", () => {
-        getWarrior(106);
+// adds event listeners to all warrior buttons
+const warriorbtn = () => {
+    warriors.forEach((warrior) => {
+        const btn = document.getElementById(`warrior-button-${warrior.id}`);
+        if (btn) {
+            btn.addEventListener("click", () => {
+                getWarrior(warrior.id);
+            });
+        }
     });
 };
 
-// Immediately invoke the showWarriors function
+// sets all events
+const setEvents = () => {
+    warriorbtn();
+};
+
+// Immediately invoke the start functions
 (() => {
-    setEvents();
     showWarriors();
+    setEvents();
 })();
