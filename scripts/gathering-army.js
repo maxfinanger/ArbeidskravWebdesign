559afc4b7b1ffa255671205020d48fc6c880c9ad
Max Dyrø Finanger <max.d.finanger@gmail.com>
2023-10-03 11:53:25 +0200

    Fixed Bootstrap for the gathering army

--- scripts/gathering-army.js
+++ scripts/gathering-army.js
@@ -5,22 +5,23 @@ const warriors = WarriorModule.getAll();
 console.log(warriors);
 
 const showWarriors = () => {
-    warriorDiv.innerHTML = "";
+    let allWarriorsHTML = "";
 
     warriors.forEach((warrior) => {
-        const warriorItem = document.createElement("li");
-        const htmlTxt = `
-            <div class="warrior">
+        const warriorHTML = `
+        <article class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="shadow border border-rounded p-1 card h-100">
                 <img src="images/${warrior.img}" alt="${warrior.name}" />
-                <div class="warrior-info">
-                    <h2>${warrior.name}</h2>
-                    <p>Health: ${warrior.health}</p>
-                </div>
+                <h2>${warrior.name}</h2>
+                <p>Health: ${warrior.health}</p>
             </div>
+        </article>
         `;
-        warriorItem.innerHTML = htmlTxt;
-        warriorDiv.appendChild(warriorItem);
+
+        allWarriorsHTML += warriorHTML;
     });
+
+    warriorDiv.innerHTML = allWarriorsHTML;
 };
 
 // Immediately invoke the showWarriors function
