b6856b6fc3130c15185b1953f596b5f54abed127
Max Dyrø Finanger <max.d.finanger@gmail.com>
2023-10-03 11:28:18 +0200

    Added to gathering army js

---
+++ scripts/gathering-army.js
@@ -0,0 +1,27 @@
+import WarriorModule from "./Modules/WarriorModule.js";
+
+const warriorDiv = document.getElementById("warrior-div");
+const warriors = WarriorModule.getAll();
+console.log(warriors);
+
+const showWarriors = () => {
+    warriorDiv.innerHTML = "";
+
+    warriors.forEach((warrior) => {
+        const warriorItem = document.createElement("li");
+        warriorItem.innerHTML = `
+            <div class="warrior">
+                <img src="img/${warrior.img}" alt="${warrior.name}" />
+                <div class="warrior-info">
+                    <h2>${warrior.name}</h2>
+                    <p>Health: ${warrior.health}</p>
+                </div>
+            </div>
+        `;
+        warriorDiv.appendChild(warriorItem);
+    });
+
+    (function () {
+        showWarriors();
+    })();
+};
