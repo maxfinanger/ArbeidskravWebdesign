f548117f5b29ce8fd7f49b1ccdbe1be626c8b3b7
mariusm1008 <mmoreite@gmail.com>
2023-10-05 13:55:37 +0200

    Oppdaterte filer

---
+++ scripts/getting-resources.js
@@ -0,0 +1,33 @@
+import LocalStorageModule from "./modules/LocalStorageModule.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Function for updating resource values on the page
+  const updateResourceCounts = () => {
+    const { gold, wood, metal } = LocalStorageModule.GetResources();
+    document.getElementById("metal-count").textContent = metal;
+    document.getElementById("gold-count").textContent = gold;
+    document.getElementById("wood-count").textContent = wood;
+  };
+
+  // Initialising resources from LocalStorageModule and updating the counters
+  updateResourceCounts();
+
+  // Mine click event
+  document.getElementById("mine-img").addEventListener("click", () => {
+    const { gold, wood, metal } = LocalStorageModule.GetResources();
+    const chance = Math.random();
+    if (chance < 0.75) {
+      LocalStorageModule.SaveResources(gold, wood, metal + 1);
+    } else {
+      LocalStorageModule.SaveResources(gold + 1, wood, metal);
+    }
+    updateResourceCounts();
+  });
+
+  // Woods click event
+  document.getElementById("woods-img").addEventListener("click", () => {
+    const { gold, wood, metal } = LocalStorageModule.GetResources();
+    LocalStorageModule.SaveResources(gold, wood + 1, metal);
+    updateResourceCounts();
+  });
+});
