72b1aa1aaef5c6db01972f0ba949bcab055286c2
Max Dyrø Finanger <max.d.finanger@gmail.com>
2023-10-06 11:22:12 +0200

    Changes comments

--- scripts/gathering-army.js
+++ scripts/gathering-army.js
@@ -1,16 +1,19 @@
+//Imports
 import WarriorModule from "./modules/WarriorModule.js";
 import LocalStorageModule from "./modules/LocalStorageModule.js";
 import EquipmentModule from "./modules/EquipmentModule.js"; // Import EquipmentModule
 
+// Variables
 const warriorDiv = document.getElementById("warrior-div");
 const warriors = WarriorModule.getAll();
 const equipments = EquipmentModule.getAll(); // Get all equipment
 
+/* ** Functions ** */
 const showWarriors = () => {
-  let allWarriorsHTML = "";
+    let allWarriorsHTML = "";
 
-  warriors.forEach((warrior) => {
-    const warriorHTML = `
+    warriors.forEach((warrior) => {
+        const warriorHTML = `
         <article class="col-12 col-md-4 col-lg-3 mb-3">
             <div class="shadow border border-rounded p-1 card h-100 d-flex flex-column justify-content-end">
                 <div class="card-content mb-auto">
@@ -23,17 +26,17 @@ const showWarriors = () => {
         </article>
         `;
 
-    allWarriorsHTML += warriorHTML;
-  });
+        allWarriorsHTML += warriorHTML;
+    });
 
-  return allWarriorsHTML;
+    return allWarriorsHTML;
 };
 
 const showEquipment = () => {
-  let allEquipmentHTML = "";
+    let allEquipmentHTML = "";
 
-  equipments.forEach((equipment) => {
-    const equipmentHTML = `
+    equipments.forEach((equipment) => {
+        const equipmentHTML = `
         <article class="col-12 col-md-4 col-lg-3 mb-3">
             <div class="shadow border border-rounded p-1 card h-100 d-flex flex-column justify-content-end">
                 <div class="card-content mb-auto">
@@ -45,143 +48,150 @@ const showEquipment = () => {
         </article>
         `;
 
-    allEquipmentHTML += equipmentHTML;
-  });
+        allEquipmentHTML += equipmentHTML;
+    });
 
-  return allEquipmentHTML;
+    return allEquipmentHTML;
 };
 
 //Gets resources from local storage and updates the counters
 const loadResources = () => {
-  const { gold, wood, metal } = LocalStorageModule.GetResources();
-  document.getElementById("gold-count").textContent = gold;
-  document.getElementById("wood-count").textContent = wood;
-  document.getElementById("metal-count").textContent = metal;
+    const { gold, wood, metal } = LocalStorageModule.GetResources();
+    document.getElementById("gold-count").textContent = gold;
+    document.getElementById("wood-count").textContent = wood;
+    document.getElementById("metal-count").textContent = metal;
 };
 
 // gets a warrior by id and adds it to the army
 const getWarrior = (id) => {
-  const warriors = WarriorModule.getAll();
-  const warrior = warriors.find((warrior) => warrior.id === id);
-  console.log(warrior);
+    const warriors = WarriorModule.getAll();
+    const warrior = warriors.find((warrior) => warrior.id === id);
+    console.log(warrior);
 
-  // Check if the player can afford the warrior
-  if (!canAffordWarrior(warrior.price)) {
-    notify("You don't have enough gold to buy this warrior.");
-    return;
-  }
+    // Check if the player can afford the warrior
+    if (!canAffordWarrior(warrior.price)) {
+        notify("You don't have enough gold to buy this warrior.");
+        return;
+    }
 
-  // Deduct gold
-  payForWarrior(warrior.price);
+    // Deduct gold
+    payForWarrior(warrior.price);
 
-  // Add the warrior to the army using the encapsulated function
-  LocalStorageModule.addWarriorToArmy(warrior);
+    // Add the warrior to the army using the encapsulated function
+    LocalStorageModule.addWarriorToArmy(warrior);
 
-  console.log(LocalStorageModule.GetArmy());
+    console.log(LocalStorageModule.GetArmy());
 };
 
 const payForWarrior = (goldPrice) => {
-  const resources = LocalStorageModule.GetResources();
-  resources.gold -= goldPrice;
-  LocalStorageModule.SaveResources(
-    resources.gold,
-    resources.wood,
-    resources.metal
-  );
+    const resources = LocalStorageModule.GetResources();
+    resources.gold -= goldPrice;
+    LocalStorageModule.SaveResources(
+        resources.gold,
+        resources.wood,
+        resources.metal
+    );
 };
 
 const canAffordWarrior = (goldPrice) => {
-  const { gold } = LocalStorageModule.GetResources();
-  return gold >= goldPrice;
+    const { gold } = LocalStorageModule.GetResources();
+    return gold >= goldPrice;
 };
 
 // adds event listeners to all warrior buttons
 const warriorbtn = () => {
-  warriors.forEach((warrior) => {
-    const btn = document.getElementById(`warrior-button-${warrior.id}`);
-    if (btn) {
-      btn.addEventListener("click", () => {
-        getWarrior(warrior.id);
-        loadResources();
-      });
-    }
-  });
+    warriors.forEach((warrior) => {
+        const btn = document.getElementById(`warrior-button-${warrior.id}`);
+        if (btn) {
+            btn.addEventListener("click", () => {
+                getWarrior(warrior.id);
+                loadResources();
+            });
+        }
+    });
 };
 
 const resetLocalStorage = () => {
-  LocalStorageModule.DeleteAll();
+    LocalStorageModule.DeleteAll();
 };
 
 // Gets equipment by id and adds it to the inventory (or any other suitable place)
 const getEquipment = (id) => {
-  const equipment = equipments.find((equip) => equip.id === id);
-  console.log(equipment);
+    const equipment = equipments.find((equip) => equip.id === id);
+    console.log(equipment);
 
-  // Check if the player can afford the equipment
-  if (!canAffordEquipment(equipment.wood, equipment.metal)) {
-    notify("You don't have enough resources to buy this equipment.");
-    return;
-  }
+    // Check if the player can afford the equipment
+    if (!canAffordEquipment(equipment.wood, equipment.metal)) {
+        notify("You don't have enough resources to buy this equipment.");
+        return;
+    }
 
-  // Deduct resources
-  payForEquipment(equipment.wood, equipment.metal);
+    // Deduct resources
+    payForEquipment(equipment.wood, equipment.metal);
 
-  // Add the equipment to the inventory
-  const inventory = LocalStorageModule.GetInventory(); // Assuming there's a GetInventory method
-  inventory.push(equipment);
-  LocalStorageModule.SaveInventory(inventory); // Assuming there's a SaveInventory method
-  console.log(LocalStorageModule.GetInventory());
+    // Add the equipment to the inventory
+    const inventory = LocalStorageModule.GetInventory(); // Assuming there's a GetInventory method
+    inventory.push(equipment);
+    LocalStorageModule.SaveInventory(inventory); // Assuming there's a SaveInventory method
+    console.log(LocalStorageModule.GetInventory());
 };
 
 const payForEquipment = (woodCost, metalCost) => {
-  const resources = LocalStorageModule.GetResources();
-  resources.wood -= woodCost;
-  resources.metal -= metalCost;
-  LocalStorageModule.SaveResources(
-    resources.gold,
-    resources.wood,
-    resources.metal
-  ); // Assuming the SaveResources method accepts all three resources
+    const resources = LocalStorageModule.GetResources();
+    resources.wood -= woodCost;
+    resources.metal -= metalCost;
+    LocalStorageModule.SaveResources(
+        resources.gold,
+        resources.wood,
+        resources.metal
+    ); // Assuming the SaveResources method accepts all three resources
 };
 
 //Checks if the user can afford the equipment
 const canAffordEquipment = (woodCost, metalCost) => {
-  const { wood, metal } = LocalStorageModule.GetResources();
-  return wood >= woodCost && metal >= metalCost;
+    const { wood, metal } = LocalStorageModule.GetResources();
+    return wood >= woodCost && metal >= metalCost;
 };
 
 // Adds event listeners to all equipment buttons
 const equipmentBtn = () => {
-  equipments.forEach((equipment) => {
-    const btn = document.getElementById(`equipment-button-${equipment.id}`);
-    if (btn) {
-      btn.addEventListener("click", () => {
-        getEquipment(equipment.id);
-        loadResources();
-      });
-    }
-  });
+    equipments.forEach((equipment) => {
+        const btn = document.getElementById(`equipment-button-${equipment.id}`);
+        if (btn) {
+            btn.addEventListener("click", () => {
+                getEquipment(equipment.id);
+                loadResources();
+            });
+        }
+    });
 };
 
 // Function to show a toast notification used for problemshooting
 const notify = (message) => {
-  const toastElement = document.querySelector(".toast .toast-body");
-  toastElement.textContent = message;
-  const toast = new bootstrap.Toast(document.querySelector(".toast"));
-  toast.show();
+    const toastElement = document.querySelector(".toast .toast-body");
+    toastElement.textContent = message;
+    const toast = new bootstrap.Toast(document.querySelector(".toast"));
+    toast.show();
 };
 
 // sets all events
 const setEvents = () => {
-  loadResources();
-  warriorbtn();
-  equipmentBtn(); // Add this to set the event listeners for equipment
+    loadResources();
+    warriorbtn();
+    equipmentBtn(); // Add this to set the event listeners for equipment
 };
 
 // Immediately invoke the start functions
 (() => {
+<<<<<<< Updated upstream
   const content = showWarriors() + showEquipment();
   warriorDiv.innerHTML = content;
   setEvents();
   //localStorage.clear();
+=======
+    const content = showWarriors() + showEquipment();
+    warriorDiv.innerHTML = content;
+    setEvents();
+    // localStorage.clear();
+>>>>>>> Stashed changes
 })();
